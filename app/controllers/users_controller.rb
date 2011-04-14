2c2ee880f31a59f4444fe8236e7475c23e3830ce
Abhishek <abhishek.patel131@gmail.com>
2011-04-14 10:10:02 +0530

    test build..

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @users = User.all
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html # index.html.rb
       format.xml  { render :xml => @users }
     end
   end
